12936caf7670db5015c7738c0d5fa1d6b3d323b5
ninow6 <ninow6@gmail.com>
2021-06-06 22:34:32 +0700

    test-driven-development

---
+++ test/unit/baseController.spec.js
@@ -0,0 +1,21 @@
+const base = require('../../controllers/baseController.js')
+const mockRequest = (body = {}) => ({ body })
+const mockResponse = () => {
+    const res = {}
+    res.json = jest.fn().mockRetrunValue(res)
+    res.status = jest.fn().mockReturnValue(res)
+    return res
+}
+describe('base.index function', () => {
+    test('res.json called with { status: true, message: "Hello World")', done => {
+        const req = mockRequest()
+        const res = mockResponse()
+        base.index(req, res)
+        expect(res.status).toBeCalledWith(200)
+        expect(res.json).toBeCalledWith({
+            status: true,
+            message: "Hello World!"
+        })
+        done()
+    })
+})
